08beb58b099ff98664e4e2d36aa1337052701351
Adrian Martinez Lopez <1065967@adm.ehu.es>
2026-01-21 18:42:42 +0100

    Persistencia db cambiada

--- create_database.py
+++ create_database.py
@@ -8,18 +8,6 @@ from langchain_huggingface import HuggingFaceEmbeddings
 FILE_PATH = "Files"
 CHROMA_PATH="chroma"
 
-#Text Splitter
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
-)
-
-#Loader function
-def load_docs():
-    loader = DirectoryLoader(FILE_PATH, glob="*.pdf", loader_cls=PyPDFLoader)
-    docs = loader.load()
-    return docs
-
 #Splitter
 splitter = RecursiveCharacterTextSplitter(
     chunk_size =1000,
@@ -27,6 +15,7 @@ splitter = RecursiveCharacterTextSplitter(
     length_function=len,
     add_start_index=True,
 )
+
 
 #Embedding model
 model_name = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
@@ -37,19 +26,32 @@ embedding_model= HuggingFaceEmbeddings(
     model_kwargs=model_kwargs,
 )
 
-#Ejecucion
-docs = load_docs()
-chunks = splitter.split_documents(docs)
+#Loader function
+def load_docs():
+    loader = DirectoryLoader(FILE_PATH, glob="*.pdf", loader_cls=PyPDFLoader)
+    docs = loader.load()
+    return docs
 
-#Database
-vector_store= Chroma.from_documents(
-    documents=chunks,
-    embedding=embedding_model,
-    persist_directory=CHROMA_PATH
-)
+def create_vector_DB(): #solo se ejecuta directamente
+    #Ejecucion
+    docs = load_docs()
+    chunks = splitter.split_documents(docs)
+
+    #Database
+    Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=CHROMA_PATH)
+
+    print("Base de datos creada correctamente")
+    
 
 def get_db():
-    return vector_store
+    db = Chroma(
+        persist_directory=CHROMA_PATH,
+        embedding_function=embedding_model
+    )
+    return db
 
-def get_embedding_model():
-    return embedding_model
+if __name__ == "__main__":
+    create_vector_DB()
