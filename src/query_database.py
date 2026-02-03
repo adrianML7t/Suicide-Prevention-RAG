638ee690148645d50ea3a20485a1e2f199ce9bad
adrianML7t <adrian.ml7t@gmail.com>
2026-02-03 13:12:43 +0100

    Filtrado de base de datos correcto con respuesta acorde

--- src/query_database.py
+++ src/query_database.py
@@ -1,9 +1,10 @@
 import sys
-import create_database1 as DB
+import create_database as DB
 from langchain_ollama import OllamaLLM
 
 # --- CONFIGURACIÓN ---
-DB_DIR = "chroma1/"
+DB_DIR1 = "chroma1/"
+DB_DIR3 = "chroma3/"
 
 def run_rag_test(model_name):
     # 1. Set LLM to input
@@ -17,11 +18,12 @@ def run_rag_test(model_name):
         print(f"Error: Modelo '{model_name}' no reconocido.")
         return
 
-    # 2. Definir base de datos
-    db = DB.get_db()
+    # 2. Definir base de datos en base a la consulta
+    query_text = input("Escribe tu consulta: ")
+    chroma_path = get_route(llm,query_text) #baja el rendimiento pasar el llm a una funcion?
+    db = DB.get_db(chroma_path)
 
     # 3. Retrieval
-    query_text = input("Escribe tu consulta: ")
     results = db.similarity_search_with_relevance_scores(query_text, k=5)
   
     context_list = []
@@ -50,7 +52,7 @@ def run_rag_test(model_name):
     # 5. Llamada al LLM
     response = llm.invoke(prompt_text)
 
- # --- Mostrar resultados ---
+ # --- Mostrar resultados --- -> pasar a show_results
     print("\n" + "="*80)
     print(f"RESULTADOS PARA EL MODELO: {model_name}")
     print("="*80)
@@ -58,9 +60,35 @@ def run_rag_test(model_name):
     print("-" * 80)
     print(f"Respuesta:\n{response}")
     print("-" * 80)
-    # CORRECCIÓN: Usamos 'full_context' en lugar de 'context'
     print(f"Contexto Utilizado (Score Top: {results[0][1] if results else 'N/A'}):\n{full_context}")
     print("="*80 + "\n")
+
+
+def get_route(llm, query): # Mas adelante introducir aqui la clasificacion de riesgo ??
+    print(f" Analizando intención de la consulta...")
+        
+    router_prompt = f"""
+    Analiza la siguiente consulta sobre suicidio y clasifícala.
+    
+    Consulta: "{query}"
+    
+    Si el usuario pide ayuda para SÍ MISMO o describe sus propios sentimientos, responde: PROPIO
+    Si el usuario pregunta cómo ayudar a OTRA PERSONA (hijo, amigo, pareja), responde: OTRO
+    
+    Responde SOLAMENTE con una de esas dos palabras (PROPIO u OTRO).
+    """
+    # El LLM decide
+    decision = llm.invoke(router_prompt).strip().upper()
+    
+    # Lógica de selección de base de datos
+    if "OTRO" in decision:
+        print(">> Modo detectado: Ayuda a un TERCERO (Familiares/Amigos)")
+        selected_db_path = DB_DIR3
+    else:
+        # Por seguridad, si duda, asumimos que es el propio usuario (PROPIO)
+        print(">> Modo detectado: Ayuda PROPIA (Paciente)")
+        selected_db_path = DB_DIR1
+    return selected_db_path
 
 if __name__ == "__main__":
     # Verificamos que se haya pasado un argumento por terminal
