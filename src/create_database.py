5b593c07297a9780eb448c393fab3381e5b9c9c8
adrianML7t <adrian.ml7t@gmail.com>
2026-02-03 12:03:43 +0100

    Create_database optimized for launch.json with 2 bdd

--- src/create_database.py
+++ src/create_database.py
@@ -3,9 +3,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
+import sys
 
-FILE_PATH = "Files/3rd"
-CHROMA_PATH = "chroma3/"
 
 def get_embedding_model():
     model_name = "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
@@ -16,12 +15,12 @@ def get_embedding_model():
         model_kwargs=model_kwargs,
     )
 
-def load_docs():
-    loader = DirectoryLoader(FILE_PATH, glob="*.pdf", loader_cls=PyPDFLoader)
+def load_docs(file_path):
+    loader = DirectoryLoader(file_path, glob="*.pdf", loader_cls=PyPDFLoader)
     docs = loader.load()
     return docs
 
-def create_vector_db():
+def create_vector_db(file_path, chroma_path):
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=500,
@@ -29,24 +28,29 @@ def create_vector_db():
         add_start_index=True,
     )
     
-    docs = load_docs()
+    docs = load_docs(file_path)
     chunks = splitter.split_documents(docs)
     embedding_model = get_embedding_model()
     
     Chroma.from_documents(
         documents=chunks,
         embedding=embedding_model,
-        persist_directory=CHROMA_PATH
+        persist_directory=chroma_path
     )
-    print("BDD creada")
+    print(f"BDD {chroma_path} creada a partir de {file_path}")
 
-def get_db():
+def get_db(chroma_path):
     embedding_model = get_embedding_model()
     vector_store = Chroma(
-        persist_directory=CHROMA_PATH,
+        persist_directory=chroma_path,
         embedding_function=embedding_model
     )
     return vector_store
 
-if __name__ == "__main__": #Nota: los imports retrasan bastante la ejecucion del programa..
-    create_vector_db()
+if __name__ == "__main__": #Nota: los imports retrasan bastante la ejecucion del programa.. Se podria solucionar?
+    if len(sys.argv) == 1:
+        print("to execute: src/create_database.py file_path chroma_path")
+    elif len(sys.argv) == 3:
+        file_path = sys.argv[1]
+        chroma_path = sys.argv[2]
+        create_vector_db(file_path, chroma_path)
