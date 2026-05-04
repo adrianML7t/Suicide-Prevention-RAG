532101f7e03568b13b7a42738832cdfcb55cf6fa
adrianML7t <adrian.ml7t@gmail.com>
2026-05-04 08:26:03 +0200

    System prompt updated

--- model-files/refresh_models.bat
+++ model-files/refresh_models.bat
@@ -1,13 +1,9 @@
 @echo off
 :: 1. Eliminar los modelos existentes de llama y command sp
 echo Eliminando modelos antiguos...
-ollama rm llama3-8b-sp
-ollama rm com35-sp
 ollama rm llama3-70b-sp
 :: 2. Crear el modelo nuevamente usando el Modelfile
 echo Creando nuevos modelos adaptados
-ollama create llama3-8b-sp -f Modelfile.llama8-sp
-ollama create com35-sp -f Modelfile.com-sp
 ollama create llama3-70b-sp -f Modelfile.llama70-sp
 
 echo Proceso finalizado.
